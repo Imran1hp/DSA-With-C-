a0eb5b6cddca2a0ffb40ee054d723b7d30b14246
ali imran <aliimranlaskar123@gmail.com>
2025-08-04 00:21:09 +0530

    b1short

--- babble_short.c
+++ babble_short.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+// normal bubble sort
 void bsort(int a[],int n){
-
-int  i,j , t;
+int  i,j,t;
 for (i=0;i<n-1;i++){
 for (j=0;j<n-i-1;j++){
 
@@ -10,14 +10,33 @@ printf("\ncheck %d and %d\n",a[j],a[j+1]);
 if (a[j]>a[j+1]){
 t=a[j];
 a[j]=a[j+1];
-a[j+1]=t;
+a[j+1]=t;}
+      }
+   }
 }
 
-}
+void b1sort (int a[],int n){
 
-}
+int i,j,t,f;
+for (i=0; i<n-1; i++){
+ f = 0;
+for (j=0 ; j <n-i-1; j++){
+   if (a[j] > a[j+1]){
+      f =1;
+      t = a[j];
+      a[j] = a[j+1];
+      a[j+1] = t;
+     
+       }
+     }
+     if(f==0){
+        break;
+           } 
+   }
+ }
 
-}
+
+
 int main (){
 
     int a[30],n,i;
@@ -29,7 +48,10 @@ int main (){
     for(i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
-    bsort(a,n);
+    for (i=0;i<n;i++){
+        printf(" %d ",a[i]);
+    }
+    b1sort(a,n);
     printf("After sorting\n");
     for (i=0;i<n;i++){
         printf("%d ",a[i]);
