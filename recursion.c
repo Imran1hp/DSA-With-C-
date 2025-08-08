54aa4789aae3f94543d0221e8339e96ecfb55f91
ali imran <aliimranlaskar123@gmail.com>
2025-08-08 09:00:00 +0530

    f2 function

--- recursion.c
+++ recursion.c
@@ -4,12 +4,26 @@ if (n>0){
     f1(--n);
     printf("%d\n",n);
 }
-
 }
+void f2 (int n){
+
+
+    if (n>0){
+        f2(--n);
+        printf("p1  %d\n",n);
+        
+        f2(--n);
+        printf("p2  %d\n",n);
+    }
+}
+
+
+
+
 int main  (){
 
-int n =5;
-f1(n);
+int n =3;
+f2(n);
 
 
     return 0;
